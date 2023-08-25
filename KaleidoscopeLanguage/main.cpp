aa6f4df18511c740cbf9cd16c85d215c34588b2d
rakan <Rakan.w10@outlook.com>
2023-08-25 20:25:31 +0300

    Initial Commit

---
+++ KaleidoscopeLanguage/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KaleidoscopeLanguage
+//
+//  Created by Rakan Alotaibi on 09/02/1445 AH.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
